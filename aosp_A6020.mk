Sameer-SD/device_lenovo_A6020

A6020: AEX bringup

aosp_A6020.mk
@@ -15,16 +15,14 @@
 $(call inherit-product, device/lenovo/A6020/full_A6020.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product-if-exists, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product-if-exists, vendor/aosp/common.mk)
 
 # Boot animation
-TARGET_SCREEN_WIDTH := 1080
-TARGET_SCREEN_HEIGHT := 1920
 TARGET_BOOTANIMATION_HALF_RES := true
+TARGET_BOOT_ANIMATION_RES := 1080
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := A6020
-PRODUCT_NAME := lineage_A6020
+PRODUCT_NAME := aosp_A6020
 
-# Unofficial build ID
-TARGET_UNOFFICIAL_BUILD_ID := HYPER
+EXTENDED_BUILD_TYPE := OFFICIAL

harry8242/android_device_lenovo_A6020

aosp_A6020.mk
@@ -15,16 +15,14 @@
 $(call inherit-product, device/lenovo/A6020/full_A6020.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product-if-exists, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product-if-exists, vendor/aosp/common.mk)
 
 # Boot animation
-TARGET_SCREEN_WIDTH := 1080
-TARGET_SCREEN_HEIGHT := 1920
 TARGET_BOOTANIMATION_HALF_RES := true
+TARGET_BOOT_ANIMATION_RES := 1080
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := A6020
-PRODUCT_NAME := lineage_A6020
+PRODUCT_NAME := aosp_A6020
 
-# Unofficial build ID
-TARGET_UNOFFICIAL_BUILD_ID := HYPER
+EXTENDED_BUILD_TYPE := UNOFFICIAL